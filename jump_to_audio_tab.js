2fbaaf46f204656e9d981fcae4272e89ccf6edb7
Rain <Rainyan@users.noreply.github.com>
2023-09-02 22:46:39 +0300

    fix format

--- jump_to_audio_tab.js
+++ jump_to_audio_tab.js
@@ -76,7 +76,7 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
         promises.push(
           browser.tabs.update(currentTab.id, {
             active: false,
-          })
+          }),
         );
       }
 
@@ -226,7 +226,7 @@ function checkContextCompat() {
           id: `${EXT_TAB_ID}placeholder`,
           title: "Test menu support",
           contexts: [context],
-        })
+        }),
       );
       CONTEXTS.push(context);
     } catch (error) {
